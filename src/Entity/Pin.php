cd258aa59157dc14f2d0f37d0a3c93ef45157d53
larousse83 <laure.cambours@gmail.com>
2020-12-05 16:46:16 +0100

    add bootstrap & use form themes & fix bug

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -46,7 +46,7 @@ class Pin
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    public function setTitle(?string $title): self
     {
         $this->title = $title;
 
@@ -58,7 +58,7 @@ class Pin
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    public function setDescription(?string $description): self
     {
         $this->description = $description;
 
